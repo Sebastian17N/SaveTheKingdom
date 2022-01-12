697e4bb5b98f0bb393459ddc1ca2901f206c4598
Sebastian17N <83965352+Sebastian17N@users.noreply.github.com>
2022-01-12 21:54:01 +0100

    fixing language errors

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class BulletType
+{
+    public Sprite Sprite;
+    public float ShootingDuration = 0.5f;
+    public float Speed = 6f;
+}
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
-{    
-
+{
+    [SerializeField]
+    float speed = 6f;
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = Vector3.right * 5f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/UnitShoot.cs
+++ Assets/Scripts/UnitShoot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitShot : MonoBehaviour
+public class UnitShoot : MonoBehaviour
 {
     [SerializeField]
     GameObject BulletPrefab;
