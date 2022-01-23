3a565592ff91f1caa3f4103c53ed072f5047ed37
Sebastian17N <83965352+Sebastian17N@users.noreply.github.com>
2022-01-23 15:55:36 +0100

    Update EnemisSpawner.cs

--- Assets/Units/Enemies/EnemisSpawner.cs
+++ Assets/Units/Enemies/EnemisSpawner.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,25 +5,21 @@ using System.Threading;
 using UnityEditor;
 using System.Text.RegularExpressions;
 using Random = UnityEngine.Random;
+using System;
 
 public class EnemisSpawner : MonoBehaviour
 {
     public List<Enemy> enemiesPrefabs;
     public Enemy enemies;
-    
+
     public float spawnInterval;
     public Transform[] spawnSpot;
 
     public void Start()
-    {        
+    {
         LoadLevel("level_1");
     }
 
-    private void Update()
-    {
-        
-
-    }
     void LoadLevel(string name)
     {
         var path = "Assets/Map/Levels/" + name + ".txt";
@@ -32,11 +27,12 @@ public class EnemisSpawner : MonoBehaviour
         var lines = Regex.Split(text, Environment.NewLine);
         StartCoroutine(SpawningCoroutine(lines));
     }
+
     public IEnumerator SpawningCoroutine(string[] text)
     {
         var spawnSpotID = text.Length;
         var numberOfEnemies = text[0].Length;
-
+                 
         for (int x = 0; x < numberOfEnemies; x++)
         {
             for (int y = 0; y < spawnSpotID; y++)
@@ -49,12 +45,12 @@ public class EnemisSpawner : MonoBehaviour
                 if (character == 'x')
                 {
                     enemies = enemiesPrefabs[Random.Range(0, enemiesPrefabs.Count)];
-                    Instantiate(enemiesPrefabs[(int)enemies.enemiesType], spawnSpot[y]);
-                }             
+                    var enemy = Instantiate(enemiesPrefabs[(int)enemies.enemiesType], spawnSpot[y]);
+                    enemy.transform.localPosition = new Vector3(0, 0, -1);
+                }
             }
             yield return new WaitForSeconds(spawnInterval);
-        }        
+        }
+        
     }
-    
-   
 }
