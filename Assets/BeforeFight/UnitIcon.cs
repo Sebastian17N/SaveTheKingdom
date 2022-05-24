80efb2a7fdfc340159ef38cba97df9d98f74183a
Piotr Zieliński <Piotr.Zielinski@ttpsc.pl>
2022-05-24 17:12:44 +0200

    Added notes how to handle putting icons into slots.

--- Assets/BeforeFight/UnitIcon.cs
+++ Assets/BeforeFight/UnitIcon.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -13,10 +13,24 @@ public class UnitIcon : MonoBehaviour, IPointerDownHandler, IPointerExitHandler,
     public bool IsUnitChecked = false;
     public Transform CanvasTransform;
     public bool IsInfoVisible = false;
+
+    private bool IsAssignedToSlotAlready = false;
+
     void Start()
     {
         HideInfoButton(false);
     }
+
+    void Update()
+	{
+        if (!IsAssignedToSlotAlready)
+		{
+            // Przesuń ikonę do slotu.
+
+            // Jeśli pozycja ikony i slotu jest identyczna, to zmień IsAssignedToSlotAlready = true;
+		}
+	}
+
     public void OnPointerClick(PointerEventData eventData)
     {
         if (!IsUnitChecked)
@@ -35,25 +49,35 @@ public class UnitIcon : MonoBehaviour, IPointerDownHandler, IPointerExitHandler,
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        
-        UnitIconDragged = Instantiate(PrefabUnitCard, new Vector3(1,1,1) , Quaternion.identity);
-        UnitIconDragged.transform.SetParent(transform.parent.transform.parent.transform);
-        UnitIconDragged.GetComponent<Image>().sprite = Sprite;
+        UnitIconDragged = Instantiate(transform.gameObject, GameObject.Find("Canvas").transform);
+        UnitIconDragged.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 100);
+        UnitIconDragged.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 100);
 
-        //var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //UnitIconDragged.transform.position = new Vector3(position.x, position.y, position.z);
-        //UnitIconDragged.transform.SetParent(CanvasTransform);
-        //UnitIconDragged.GetComponent<SpellDraggedManager>().SpellPrefab = transform.gameObject;
+        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        UnitIconDragged.transform.position = new Vector3(position.x, position.y, position.z);
     }
+
+    public void OnPointerDragged()
+	{
+        // Przesuwaj ikonę za myszką.
+	}
+
+    public void OnPointerUp()
+	{
+        // 1. Znajdź pusty slot - czy w ogóle, taki istnieje.
+        // 1.a jeśli nie to zakończ, i zniknij ikonkę - zniszcz obiekt.
+        // 2. Przypisz ikonę do pustego slotu, odpal animację poruszania, lub przesuwaj ikonę na Update do czasu, aż pozycja ikony nie zrówna się z pozycją slotu.
+	}
+
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.localScale = new Vector3(1, 1, 1);
         IsUnitChecked = false;
     }
+
     public void HideInfoButton(bool isInfoButtonVisible)
     {
         var infoButton = GameObject.Find("InfoButton");   
         infoButton.SetActive(isInfoButtonVisible); 
     }
-
 }
