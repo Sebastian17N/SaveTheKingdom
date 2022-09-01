a7a9fa536514578c288d9b6d193f4a09e39f8fba
Sebastian17N <83965352+Sebastian17N@users.noreply.github.com>
2022-09-01 12:15:58 +0200

    start to create use spell radius

--- Assets/Map/Scripts/BackgroundManager.cs
+++ Assets/Map/Scripts/BackgroundManager.cs
@@ -8,6 +8,9 @@ namespace Assets.Map.Scripts
 		public int BoardWidth;
 		public int BoardHeight;
 		public Vector2 FieldLocation = Vector2.one;
+		private Vector3 TargetPosition;
+		public int X { get; private set; }
+		public int Y { get; private set; }
 
 		void Start()
 		{
@@ -28,6 +31,31 @@ namespace Assets.Map.Scripts
 		{
 			var field = Instantiate(FieldPrefab, transform);
 			field.transform.localPosition += new Vector3(x * FieldLocation.x, y * FieldLocation.y, 0);
-		}	
+		}
+
+		public void Configurate(int x, int y)
+		{
+			X = x;
+			Y = y;
+
+			TargetPosition = GetFieldPosition(x, y);
+		}
+		public Vector2 GetFieldPosition(int x, int y)
+		{
+			var basePosition = new Vector2(
+				x - BoardWidth / 2 + 0.5f,
+				y - BoardHeight / 2 + 0.5f);
+
+			return basePosition;
+		}
+		public bool IsNeighbour(BackgroundManager field)
+		{
+			if (Mathf.Abs(X - field.X) > 1f)
+				return false;
+			if (Mathf.Abs(Y - field.Y) > 1f)
+				return false;
+
+			return true;
+		}
 	}
 }
